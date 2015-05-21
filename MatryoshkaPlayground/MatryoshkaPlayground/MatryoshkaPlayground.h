e56e1a530cfb091cf6db824681859f0457115fbf
Felix Jendrusch <felix@felixjendrusch.is>
2015-05-21 14:51:14 +0200

    Cleanup playground umbrella header

--- MatryoshkaPlayground/MatryoshkaPlayground/MatryoshkaPlayground.h
+++ MatryoshkaPlayground/MatryoshkaPlayground/MatryoshkaPlayground.h
@@ -1,19 +1,9 @@
-//
-//  MatryoshkaPlayground.h
-//  MatryoshkaPlayground
-//
-//  Created by Felix Jendrusch on 5/20/15.
 //  Copyright (c) 2015 Felix Jendrusch. All rights reserved.
-//
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for MatryoshkaPlayground.
 FOUNDATION_EXPORT double MatryoshkaPlaygroundVersionNumber;
 
 //! Project version string for MatryoshkaPlayground.
 FOUNDATION_EXPORT const unsigned char MatryoshkaPlaygroundVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MatryoshkaPlayground/PublicHeader.h>
-
-
